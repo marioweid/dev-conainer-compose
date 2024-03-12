6ae899f823d28460ea35184bc91062c6f520f5cf
marioweid <mario.weidner@gmx.de>
2024-03-12 07:57:10 +0100

    changed logic still not working

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,54 +1,81 @@
 use async_trait::async_trait;
+use pingora::http::ResponseHeader;
 use pingora::prelude::*;
+use pingora::prelude::http_proxy_service;
 use std::sync::Arc;
+// use pingora_proxy::ProxyHttp;
 
 pub struct LB(Arc<LoadBalancer<RoundRobin>>);
 
+pub struct MyGateway {}
+
 #[async_trait]
-impl ProxyHttp for LB {
+impl ProxyHttp for MyGateway {
     /// For this small example, we don't need context storage
     type CTX = ();
     fn new_ctx(&self) -> () {
         ()
     }
 
+    async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool> {
+        if !session.req_header().uri.path().starts_with("/frontend")
+        {
+            let _ = session.respond_error(403).await;
+            // true: early return as the response is already written
+            return Ok(true);
+        }
+        Ok(false)
+    }
+
     async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
-        let upstream = self
-            .0
-            .select(b"", 256) // hash doesn't matter for round robin
-            .unwrap();
+        let addr: (&str, u16) = if _session.req_header().uri.path().starts_with("/frontend") {
+            ("127.0.0.1", 3000u16)
+        } else {
+            ("localhost", 3000u16)
+        };
+        // let addr: (&str, u16) =("frontend:3000", 443u16);
 
-        println!("upstream peer is: {:?}", upstream);
+        println!("connecting to {addr:?}");
 
-        // Set SNI to one.one.one.one
-        let peer = Box::new(HttpPeer::new(upstream, true, "one.one.one.one".to_string()));
+        let peer: Box<HttpPeer> = Box::new(HttpPeer::new(addr, true, "127.0.0.1".to_string()));
         Ok(peer)
     }
 
-    // async fn upstream_request_filter(
-    //     &self,
-    //     _session: &mut Session,
-    //     upstream_request: &mut RequestHeader,
-    //     _ctx: &mut Self::CTX,
-    // ) -> Result<()> {
-    //     upstream_request.insert_header("Host", "one.one.one.one").unwrap();
-    //     Ok(())
-    // }
+    async fn response_filter(
+        &self,
+        _session: &mut Session,
+        upstream_response: &mut ResponseHeader,
+        _ctx: &mut Self::CTX,
+    ) -> Result<()>
+    where
+        Self::CTX: Send + Sync,
+    {
+        // replace existing header if any
+        upstream_response
+            .insert_header("Server", "MyGateway")
+            .unwrap();
+        // because we don't support h3
+        upstream_response.remove_header("alt-svc");
+
+        Ok(())
+    }
+
 }
 
 fn main() {
     let mut my_server: Server = Server::new(None).unwrap();
     my_server.bootstrap();
 
-    let upstreams: LoadBalancer<
-        pingora::lb::selection::weighted::Weighted<pingora::lb::selection::algorithms::RoundRobin>,
-    > = LoadBalancer::try_from_iter(["frontend:3000"]).unwrap();
+    let mut my_proxy: pingora::services::listening::Service<pingora::proxy::HttpProxy<MyGateway>> = http_proxy_service(
+        &my_server.configuration,
+        MyGateway {},
+    );
 
-    let mut lb: pingora::services::listening::Service<pingora::proxy::HttpProxy<LB>> =
-        http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
-    lb.add_tcp("0.0.0.0:8081");
+    // let mut lb: pingora::services::listening::Service<pingora::proxy::HttpProxy<LB>> =
+    //     http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
+    my_proxy.add_tcp("0.0.0.0:8081");
 
-    my_server.add_service(lb);
+    my_server.add_service(my_proxy);
 
     my_server.run_forever();
 }
